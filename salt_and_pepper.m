e5eed44244cd28813aa5f421e13682abc81f5a64
Abhiejit Keshavan <137376181+abhiejiit@users.noreply.github.com>
2024-06-26 16:25:51 +0530

    Add files via upload

---
+++ salt_and_pepper.m
@@ -0,0 +1,26 @@
+% Read the original image
+original_image = imread('/Users/jith/Downloads/IMG_5975.jpg');
+
+% Display the original image
+figure;
+subplot(2, 2, 1);
+imshow(original_image);
+title('Original Image');
+
+% Display histogram of the original image
+subplot(2, 2, 2);
+imhist(original_image);
+title('Histogram of Original Image');
+
+% Add salt and pepper noise
+noisy_image = imnoise(original_image, 'salt & pepper', 0.45);
+
+% Display the noisy image
+subplot(2, 2, 3);
+imshow(noisy_image);
+title('Noisy Image');
+
+% Display histogram of the noisy image
+subplot(2, 2,4);
+imhist(noisy_image);
+title('Histogram of Noisy Image');
